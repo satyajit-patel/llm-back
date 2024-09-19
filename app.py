eaf14822ac236382f95488117e66332a22d7e785
satyajitPatel <thriver1988@gmail.com>
2024-09-19 10:09:56 +0530

    add gunicorn

--- app.py
+++ app.py
@@ -1,20 +1,42 @@
+# set up
+# > mkdir myproject
+# > cd myproject
+
+# // once
+# > python -3 -m venv .venv
+
+# > .venv\Scripts\activate
+# // run
+# flask --app index run
+# deactivate // come out from .venv
+
+# // dependencies
+# pip install Flask
+# pip install python-dotenv
+# https://ai.google.dev/api?lang=python
+# pip install -q -U google-generativeai
+# pip install gunicorn
+# pip freeze > requirements.txt
+
+
 import os
-from flask import Flask, request, Response, g, render_template, jsonify
-import marko
+from flask import Flask, request, jsonify
 import requests
-
-
 import google.generativeai as genai
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from flask_cors import CORS
+
 load_dotenv()
 genai.configure(api_key=os.getenv("API_KEY"))
 
 app = Flask(__name__)
 
+# Initialize CORS
+CORS(app)
+
 def scrape_web_content(query):
     url = f"https://www.google.com/search?q={query}"
-    # print("step1")
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     
@@ -31,10 +53,6 @@ def generate_response():
     
     # Step 1: Scrape web content based on user query
     content = scrape_web_content(query)
-    # print("step2")
-    # print("****")
-    # print(content)
-    # print("****")
     
     try:
         # Step 2: Generate a response using LLM
@@ -48,3 +66,4 @@ def generate_response():
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
